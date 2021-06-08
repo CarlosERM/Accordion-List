a218d7e80ac7acce9a8d3cb9f364ec9cc0e7f581
Carlos Eduardo Rocha Miranda <74724103+CarlosERM@users.noreply.github.com>
2021-06-08 20:07:00 -0300

    quase la

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function accordion() {
 }
 
 accordion();
+
+// Can i do this?
+
+const body = document.querySelector("body");
+const imageWoman = document.querySelector(".box__woman");
+const imageBack = document.querySelector(".box__back");
+const box__image = document.querySelector(".box__image");
+
+function mudaImagem(event) {
+  if (event.currentTarget.innerWidth >= 768) {
+    imageWoman.src = "images\\illustration-woman-online-desktop.svg";
+    imageBack.src = "images\\bg-pattern-desktop.svg";
+  } else {
+    imageWoman.src = "images\\illustration-woman-online-mobile.svg";
+    imageBack.src = "images\\bg-pattern-mobile.svg";
+  }
+}
+window.addEventListener("resize", mudaImagem);
